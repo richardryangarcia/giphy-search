c9b82c1c3372e3dd12f6698079d8673fb1831452
Richard Garcia <richard@innervent.net>
2019-05-16 20:22:03 -0500

    set up routing and redux store with middlewares

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//store
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {createHashHistory} from 'history';
+
+//middleware
+import {routerMiddleware} from 'connected-react-router';
+import createSagaMiddleware from 'redux-saga';
+
+//tools
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+import Router from './router';
+import reducers from './redux/reducers';
+// import sagas from './redux/sagas';
+
+// create store with middlewares
+const history = createHashHistory();
+const sagaMiddleware = createSagaMiddleware();
+const routeMiddleware = routerMiddleware(history);
+const middlewares = [sagaMiddleware, routeMiddleware];
+const store = createStore(reducers(history), composeWithDevTools(applyMiddleware(...middlewares)));
+
+// sagaMiddleware.run(sagas)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history} />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/router.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import {ConnectedRouter} from 'connected-react-router';
+
+//import app pages
+import App from './App';
+import NotFound from './NotFound';
+
+//define routes
+const routes = [
+  {
+    path: '/home',
+    component: App,
+    exact: true
+  },
+];
+
+class Router extends React.Component {
+  render(){
+    const {history} = this.props;
+    return (
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/home" />}/>
+          {
+            routes.map(route => (
+              <Route
+                path={route.path}
+                component={route.component}
+                key={route.path}
+                exact={route.exact}
+              />
+            ))
+          }
+          <Route component={NotFound} />
+        </Switch>
+      </ConnectedRouter>
+    );
+  }
+}
+
+export default Router;
